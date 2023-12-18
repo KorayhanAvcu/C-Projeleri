fe6c1ef41843c91b40ef52623fa6cf6ad873efce
KorayhanAvcu <84989228+KorayhanAvcu@users.noreply.github.com>
2023-12-18 15:37:35 +0300

    Add files via upload

---
+++ ucgen/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.Write("Ucgen cizimi icin boyut giriniz:");
+int boyut = int.Parse(Console.ReadLine());
+Ucgen(boyut);
+
+static void Ucgen(int boyut){
+    for (int i = 1; i <= boyut; i++)
+    {
+        for (int k = 1; k <= boyut-i; k++)
+        {
+            Console.Write(" ");
+        }
+        for (int j = 1; j <= (2*i-1); j++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+        
+    }
+}
