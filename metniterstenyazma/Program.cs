3acb6cddd1e1054abc197059aebd5b7a29290214
KorayhanAvcu <84989228+KorayhanAvcu@users.noreply.github.com>
2023-12-18 16:08:06 +0300

    Add files via upload

---
+++ metniterstenyazma/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Metin Giriniz: ");
+string metin=Console.ReadLine();
+string gcc="";
+for (int i = metin.Length-1; i >= 0; i--)
+{
+    gcc+=metin[i];
+}
+Console.WriteLine("Tersi : " + gcc);
