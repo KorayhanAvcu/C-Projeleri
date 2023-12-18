12d9a615a898dac90d2ddbfd07d31f8473300010
KorayhanAvcu <84989228+KorayhanAvcu@users.noreply.github.com>
2023-12-18 15:54:22 +0300

    Add files via upload

---
+++ algoritma/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Kelime giriniz:");
+string metin = Console.ReadLine();
+Console.Write("Sayi Giriniz:");
+int sayi=int.Parse(Console.ReadLine());
+string gcc="";
+for (int i = 0; i < metin.Length; i++)
+{
+    if(i==sayi)
+        continue;
+    else{
+        gcc += metin[i];
+    }
+        
+}
+Console.WriteLine("Sonuc : " + gcc);
