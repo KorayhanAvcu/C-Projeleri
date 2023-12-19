ab925ce95b58e2991d4ba9667cb28cde7bb307e4
KorayhanAvcu <84989228+KorayhanAvcu@users.noreply.github.com>
2023-12-19 09:52:49 +0300

    Add files via upload

---
+++ integer_ikilileri/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Kac tane integer ikili sayi gireceksiniz?  : ");
+int n = int.Parse(Console.ReadLine());
+int[] toplam = new int[n];
+int s1,s2,sonuc;
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Sayi Giriniz: ");
+    s1=int.Parse(Console.ReadLine());
+    Console.Write("Sayi Giriniz: ");
+    s2=int.Parse(Console.ReadLine());
+    sonuc = ikili(s1,s2);
+    toplam[i]=sonuc;
+
+}
+for (int i = 0; i < toplam.Length; i++)
+{
+    Console.Write(toplam[i] + " ");
+}
+static int ikili(int s1,int s2){
+    if(s1==s2)
+        return (s1*s2)*(s1*s2);
+    else
+        return s1+s2;
+}
