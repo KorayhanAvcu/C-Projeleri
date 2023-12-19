c0ed8762413f9108e828b432f1ca811d3fb1a62d
KorayhanAvcu <84989228+KorayhanAvcu@users.noreply.github.com>
2023-12-19 08:17:08 +0300

    Add files via upload

--- metniterstenyazma/Program.cs
+++ metniterstenyazma/Program.cs
@@ -1,8 +1,22 @@
-﻿Console.Write("Metin Giriniz: ");
-string metin=Console.ReadLine();
-string gcc="";
-for (int i = metin.Length-1; i >= 0; i--)
-{
-    gcc+=metin[i];
-}
-Console.WriteLine("Tersi : " + gcc);
+﻿
+Console.WriteLine("Bir string ifade giriniz:");
+string metin = Console.ReadLine();
+
+string sonuc = Degistir(metin);
+Console.WriteLine("Sonuç: " + sonuc);
+    
+
+static string Degistir(string metin){
+    char[] charDizisi = metin.ToCharArray();
+    char temp;
+    for (int i = 0; i < charDizisi.Length-1; i++)
+    {
+        
+        temp = charDizisi[i+1];
+        charDizisi[i+1]=charDizisi[i];
+        charDizisi[i] = temp;
+        
+    }
+    return new string(charDizisi);
+
+} 
